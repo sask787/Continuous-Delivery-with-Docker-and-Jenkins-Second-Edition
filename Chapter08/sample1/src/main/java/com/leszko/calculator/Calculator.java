d315d43f87820fcd16ae2004eddbb8b77015e5af
sask787 <51383731+sask787@users.noreply.github.com>
2023-02-18 21:59:56 -0500

    Added comments to calculator.java

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -3,6 +3,10 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+/**
+ * Calculator Application.
+ */
+
 @Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
